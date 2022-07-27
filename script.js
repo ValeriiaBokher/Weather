179524223ca6d173746585ce212ede2f01e976dc
ValeriiaBokher <valeriiabokher@gmail.com>
2022-07-27 19:02:08 +0300

    Week7_Final

--- script.js
+++ script.js
@@ -99,11 +99,15 @@ function ShowResult(response) {
 	document.querySelector("#hum").innerHTML = response.data.main.humidity;
 	document.querySelector("#weather").innerHTML =
 		response.data.weather[0].main;
-
 	let temperatureElement = document.querySelector("#celsius");
 	celsiusTemperature = response.data.main.temp;
 	temperatureElement.innerHTML = Math.round(celsiusTemperature);
 	document.querySelector("#degree").innerHTML = `&deg;`;
+	let iconElement = document.querySelector("#icon");
+	iconElement.setAttribute(
+		"src",
+		`GIF/Linear/${response.data.weather[0].icon}.gif`
+	);
 }
 
 function ShowPosition(position) {
